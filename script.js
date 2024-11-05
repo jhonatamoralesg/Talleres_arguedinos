0e45e5e956730b3e88429a4a5333e1bf308e9518
CoperAgresive <jhonatan.morales@tecsup.edu.pe>
2024-11-05 13:40:17 -0800

    cambios git2

--- script.js
+++ script.js
@@ -13,34 +13,34 @@ const coursesByCategory = {
             description: "Este taller está diseñado para jovenes curiosos  que quieren aprender los fundamentos de la electricidad y explorar  como funciona el mundo electrico",
             icon: "code-2",
             image: "img/electricidad.jpeg",
-            price: "S/. 299"
+            price: "S/. 220"
         },
         {
             title: "Instalación de Cámaras",
             description: "El taller de instalación  de redes de cámaras  te enseñará a crear conexiones y a  instalar cámaras de seguridad, paso a paso. Aprenderás a usar todo tipo de herramientas y realizar conexiones",
             icon: "cpu",
             image: "img/camaras.jpeg",
-            price: "S/. 349"
+            price: "S/. 200"
         },
         {
             title: "Tech Lab",
             description: "Los estudiantes de Secundaria  aprenderán a ensamblar  una computadora desde cero y descubriran como resolver problemas técnicos básicos",
             icon: "database",
             image: "img/computadoras.jpeg",
-            price: "S/. 319"
+            price: "S/. 200"
         },
         {
             title: "Robotica",
             description: "Los estudiantes de secundaria exploraran la robotica, diseñando  y construyendo robots. Este taller es práctico y apto para todos los que estén interesados en la ciencia e ingeniería",
             icon: "database",
             image: "img/robotica.jpeg",
-            price: "S/. 319"
+            price: "S/. 200"
         },   {
             title: "Diseño",
             description: "Los estudiantes desccubrirán el emocinante mundo del diseño gráfico, aprendiendo a crear desde logos y posters hasta piezas digitales para redes y mucho más ",
             icon: "database",
             image: "img/diseño.jpeg",
-            price: "S/. 319"
+            price: "S/. 180"
         }
     ],
     art: [
@@ -49,21 +49,21 @@ const coursesByCategory = {
             description: "Bienvenidos a Danza en Movimiento, Este taller es el lugar perfecto para los adolescentes que desean expresarse a través del movimiento y la música.",
             icon: "brush",
             image: "img/danza.jpeg",
-            price: "S/. 259"
+            price: "S/. 130"
         },
         {
             title: "Taller de Cocina",
             description: "Descubre el chef que llevas dentro ! En este taller  aprenderás a preparar platillos deliciosos, sencillos y llenos  de sabor  que sorprenderán a tu familia.",
             icon: "camera",
             image: "img/cocina.jpeg",
-            price: "S/. 289"
+            price: "S/. 180"
         },
         {
             title: "Arte Creativo",
             description: "Este taller està diseñado para estudiantes que quieren liberar su imaginaciòn y experimentar con diferentes formas de arte",
             icon: "camera",
             image: "img/artecreativo.jpeg",
-            price: "S/. 289"
+            price: "S/. 150"
         },
        
     ],
@@ -73,7 +73,7 @@ const coursesByCategory = {
             description: "Los adolescentes se adentrarán en el emocinante mundo de las artes marciales mixtas(MMA),  una disciplina  que combina fuerza, agilidad y autocontrol",
             icon: "dumbbell",
             image: "img/artesmarciales.jpeg",
-            price: "S/. 199"
+            price: "S/. 180"
         }
       
     ]
